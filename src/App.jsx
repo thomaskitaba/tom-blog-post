017d84004ec02c0f43bffe8d72db9536021d738e
thomaskitaba <thomas.kitaba@gmail.com>
2024-02-12 23:22:09 +0300

    setup everything googd WORKING STAGE

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import MyContextProvider from './components/MyContextProvider';
+import './App.css';
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Notification } from './components/Notification';
+import Posts  from './components/Posts';
+import NavBar2 from './components/NavBar2';
+import Sqlite from './components/Sqlite';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [count, setCount] = useState(0)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <MyContextProvider>
+        <NavBar />
+        <Sqlite />
+        <Banner />
+        <Notification />
+        <Posts />
+    </MyContextProvider>
+
     </>
   )
 }
-
 export default App
