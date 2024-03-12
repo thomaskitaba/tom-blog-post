f6f1c77012ebbf65fe3d78513f5406ba2128beac
thomaskitaba <thomas.kitaba@gmail.com>
2024-03-12 12:16:58 +0300

    update

--- server.js
+++ server.js
@@ -444,7 +444,7 @@ app.post('/api/sendemail', async (req, res) => {
 });
 
 app.post('/api/test', async (req, res) => {
-  res.json{test: 'success'};
+  res.json({test: 'success'});
 });
 // todo: end of test send email
 
